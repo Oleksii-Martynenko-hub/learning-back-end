8f6539c49a918f8504eb98c79f6a43f6fe2370a2
Martynenko <4lexm4rtin@gmail.com>
2021-04-04 14:09:27 +0300

    Added netlify lambda

---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+
+const body = require('./body.json');
+
+const app = express();
+
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+router.post('/add-user', (req, res, next) => {
+  body.users.push(req.body.username);
+  fs.writeFileSync('body.json', JSON.stringify(body, null, 2));
+
+  res.redirect('/');
+});
+
+router.get('/users', (req, res, next) => res.send(body));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
