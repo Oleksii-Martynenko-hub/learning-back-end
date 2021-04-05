a7c2212dea4dc676110da8f12eb1a4ba77979a57
Martynenko <4lexm4rtin@gmail.com>
2021-04-05 14:19:23 +0300

    server test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 
-const body = require('../body.json');
+const body = require('./body.json');
 
 const app = express();
 
