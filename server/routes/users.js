bb1812bc8a5cde58d1544d0378967a60b8e48e06
Martynenko <4lexm4rtin@gmail.com>
2021-04-06 16:51:37 +0300

    test

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,11 +4,11 @@ const usersControllers = require('../controllers/users')
 
 const router = express.Router();
 
-router.post('/add-user', usersControllers.postAddUser);
-// router.post('/add-user', (req, res, next) => {
-//   const user = req.body;
-//   res.json(user);
-// });
+// router.post('/add-user', usersControllers.postAddUser);
+router.post('/add-user', (req, res, next) => {
+  const user = req.body;
+  res.json(user);
+});
 
 router.get('/users', usersControllers.getUsers);
 
