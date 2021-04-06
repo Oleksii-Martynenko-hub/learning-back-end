f3707779471dfe730ab482b94b2d247f40383a45
Martynenko <4lexm4rtin@gmail.com>
2021-04-06 13:06:52 +0300

    test

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/users');
 exports.postAddUser = (req, res, next) => {
   const user = new User(req.body);
   const newUser = user.save();
-  res.send(newUser);
+  res.status(201);
 };
 
 exports.getUsers = (req, res, next) => res.send(User.fetchAll());
